87f94835d17dd673f39ade3aec62bbec160cc92f
Cs07041997 <84439789+Cs07041997@users.noreply.github.com>
2021-07-18 23:19:58 +0530

    Algorithm

---
+++ Day_16_17_DataStructure/src/com/bridgelabz/program/AnagramAndPalindromePrimeNumbers.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.program;
+
+import com.bridgelabz.utility.AlgorithmUtility;
+
+public class AnagramAndPalindromePrimeNumbers {
+	public static void main(String[] args) {
+		AlgorithmUtility utility = new AlgorithmUtility();
+		
+		utility.getPrimeNumbers();
+	}
+}
